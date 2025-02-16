8ffe812e5ceee22716899173df2ebda7abf40620
신광수 <sks@singwangsuui-MacBookPro.local>
2025-02-16 16:51:38 +0900

    병원소개-의료진소개 이미지 삽입

--- app/(menus)/about/medical-team/page.tsx
+++ app/(menus)/about/medical-team/page.tsx
@@ -1,7 +1,46 @@
+import Image from "next/image";
+
 export const metadata = {
   title: "의료진소개",
 };
 
 export default function Introduce() {
-  return <div>의료진소개</div>;
+  return (
+    <div>
+      <div className="relative w-full aspect-[1920/550]">
+        <Image
+          src="/02_about/top_img_01.jpg"
+          alt="메인 이미지"
+          fill
+          style={{ objectFit: "contain", objectPosition: "center" }}
+        />
+      </div>
+      <div className="w-[80%] mx-auto">
+        <div className="relative w-full aspect-[1536/928]">
+          <Image
+            src="/02_about/medical-team/Dr_01.jpg"
+            alt="메인 이미지"
+            fill
+            style={{ objectFit: "contain", objectPosition: "center" }}
+          />
+        </div>
+        <div className="relative w-full aspect-[1536/928]">
+          <Image
+            src="/02_about/medical-team/Dr_02.jpg"
+            alt="메인 이미지"
+            fill
+            style={{ objectFit: "contain", objectPosition: "center" }}
+          />
+        </div>
+        <div className="relative w-full aspect-[1536/928]">
+          <Image
+            src="/02_about/medical-team/Dr_03.jpg"
+            alt="메인 이미지"
+            fill
+            style={{ objectFit: "contain", objectPosition: "center" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
